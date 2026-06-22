570ab013023f3cf0f98efe470260176e6d48b66c
Kuramenai <77842197+Kuramenai@users.noreply.github.com>
2026-06-22 08:59:26 +0800

    Fix bugs

--- run_script.sh
+++ run_script.sh
@@ -8,8 +8,22 @@
 # python inference.py -use-context -place_name $1 -path_type highway_free | tee -a inference_log.txt
 # python evaluate.py -place_name $1 -path_type $2  -$3
 
-python subgraph_construction.py    -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task rank_contexts -top_k 9 -top_k_shortest
-python context_generation.py       -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task rank_contexts -top_k 9
-python prompt_generation.py        -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task rank_contexts -top_k 9
-python inference.py                -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task rank_contexts -top_k 9
-python evaluate_ranked_contexts.py -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task rank_contexts -top_k 9
+# python subgraph_construction.py    -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9 -top_k_shortest
+# python context_generation.py       -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9
+# python prompt_generation.py        -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9
+# python inference.py                -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9
+# python evaluate_paper_v2.py        -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9
+
+
+# python context_generation.py -use-context -place_name $1 -path_type poi_aware -corridor_graph_form uncompressed
+# python prompt_generation.py  -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task route_segments -top_k 9 -corridor_graph_form uncompressed
+# python inference.py          -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task route_segments -top_k 9 -corridor_graph_form uncompressed
+# python evaluate_paper_v2.py    -use-context -place_name $1 -path_type poi_aware -retrieval spatial_hybrid -llm_task route_segments -top_k 9 -corridor_graph_form uncompressed
+
+python context_generation.py -use-context -place_name beijing -path_type poi_aware -corridor_graph_form uncompressed
+
+python prompt_generation.py -use-context -place_name beijing -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9 -corridor_graph_form uncompressed
+
+python inference.py -use-context -place_name beijing -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9 -corridor_graph_form uncompressed
+
+python evaluate_paper_v2.py -use-context -place_name beijing -path_type poi_aware -retrieval spatial_hybrid -llm_task anchor_segments -top_k 9 -corridor_graph_form uncompressed
